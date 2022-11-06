0bbe9b1789422cc4d8ef9df3a4ba42d991385fe1
Andrei Nevzorov <vzor.mobile@gmail.com>
2022-11-07 00:20:53 +0400

    modified:   Task_2_4.sql

--- Task_2_4.sql
+++ Task_2_4.sql
@@ -20,4 +20,28 @@ SET @id=
 FROM employees.work_results
 ) as t2 ON t1.id=t2.employee_id ORDER BY t2.bonus DESC LIMIT 1);
 SELECT name FROM departments WHERE id = @id;
-# Проиндексируйте зарплаты сотрудников с учетом коэффициента премии. Для сотрудников с коэффициентом премии больше 1.2 – размер индексации составит 20%, для сотрудников с коэффициентом премии от 1 до 1.2 размер индексации составит 10%. Для всех остальных сотрудников индексация не предусмотрена.
+# Проиндексируйте зарплаты сотрудников с учетом коэффициента премии. 
+# Для сотрудников с коэффициентом премии больше 1.2 – размер индексации составит 20%, 
+# для сотрудников с коэффициентом премии от 1 до 1.2 размер индексации составит 10%. 
+# Для всех остальных сотрудников индексация не предусмотрена.
+ALTER TABLE `employees`.`employees` 
+CHANGE COLUMN `bonus` `bonus` DECIMAL(5,2) NULL DEFAULT NULL ;
+ALTER TABLE `employees`.`employees` 
+ADD COLUMN `salary_index` DECIMAL(5,2) NULL AFTER `pay_level`;
+CREATE TEMPORARY TABLE `tmp_table` (
+`id` int(11),
+`bonus` DECIMAL(5,2));
+INSERT INTO `tmp_table`
+(SELECT t1.id, t2.bonus FROM employees as t1 JOIN
+(SELECT employee_id, 
+ (CASE  WHEN `Q1`='A' THEN '0.2'  WHEN `Q1`='B' THEN '0.1' WHEN `Q1`='C' THEN '0' WHEN `Q1`='D' THEN '-0.1' WHEN `Q1`='E' THEN '-0.2' END) +
+ (CASE  WHEN `Q2`='A' THEN '0.2'  WHEN `Q2`='B' THEN '0.1' WHEN `Q2`='C' THEN '0' WHEN `Q2`='D' THEN '-0.1' WHEN `Q2`='E' THEN '-0.2' END) +
+ (CASE  WHEN `Q3`='A' THEN '0.2'  WHEN `Q3`='B' THEN '0.1' WHEN `Q3`='C' THEN '0' WHEN `Q3`='D' THEN '-0.1' WHEN `Q3`='E' THEN '-0.2' END) +
+ (CASE  WHEN `Q4`='A' THEN '0.2'  WHEN `Q4`='B' THEN '0.1' WHEN `Q4`='C' THEN '0' WHEN `Q4`='D' THEN '-0.1' WHEN `Q4`='E' THEN '-0.2' END) as bonus
+FROM employees.work_results
+) as t2 ON t1.id=t2.employee_id);
+UPDATE employees a, tmp_table b SET a.bonus=b.bonus+1 WHERE a.id=b.id;
+DROP TABLE `tmp_table`;
+UPDATE employees.employees SET salary_index = pay_level * 1.2 WHERE bonus >= 1.2;
+UPDATE employees.employees SET salary_index = pay_level * 1.1 WHERE bonus >= 1.1 and bonus < 1.2;
+UPDATE employees.employees SET salary_index = pay_level WHERE bonus < 1.1;
